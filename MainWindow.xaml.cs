08a3e6d9eaaf42c89d13a603ced630faaf5e6ab2
Igal Leibovich <Igall@CITYSHOB.LOCAL>
2020-08-22 13:44:04 +0300

    Events

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using EventTypesDemo.ViewModels;
+using System.Windows;
+
+namespace EventTypesDemo
+{
+    public partial class MainWindow : Window
+    {
+        private EventTypesViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new EventTypesViewModel();
+            DataContext = _viewModel;
+        }
+    }
+}
